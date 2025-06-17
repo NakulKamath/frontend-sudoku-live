05b6793146613ac043950650b2c0111519af264b
Nakul Kamath <69424522+NakulKamath@users.noreply.github.com>
2025-06-17 17:03:52 +0530

    Initial layour config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SocketProvider } from "./components/Socket";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <SocketProvider>
+          <div className="container mx-auto p-4">
+            {children}
+          </div>
+        </SocketProvider>
       </body>
     </html>
   );
